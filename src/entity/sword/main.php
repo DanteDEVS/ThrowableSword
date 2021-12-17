135802696dd69ba3b89090d7c67771a7cb10b7af
DEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-12-17 16:45:54 +0200

    Update main.php

--- src/entity/sword/main.php
+++ src/entity/sword/main.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace entity\sword;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{ 
+
+  public function onEnable(){
+    $this->getLogger()->info("ThrowableSword Enabled");
+  }
+
+}
