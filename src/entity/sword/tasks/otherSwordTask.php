34fc421e6fe8b50230492b61598905216bc8f5ef
DEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-12-17 18:13:55 +0200

    test

--- src/entity/sword/tasks/otherSwordTask.php
+++ src/entity/sword/tasks/otherSwordTask.php
@@ -18,6 +18,7 @@ class otherSwordTask extends Task{
         if(!$this->sword->isClosed()){
             foreach($this->plugin->players as $player){
                 if($this->sword->asVector3()->distance($player) < 5){
+                    $this->plugin->plugin->getScheduler()->scheduleDelayedTask(new despawnsword($this->sword), 0);
                     }
                 }
             }
