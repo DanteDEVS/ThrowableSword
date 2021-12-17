824ac8ddfd60fe27a6738e22f42b650e877e289f
DEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-12-17 17:04:44 +0200

    Changed Main.php

--- src/entity/sword/Main.php
+++ src/entity/sword/Main.php
@@ -3,11 +3,23 @@
 namespace entity\sword;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\entity\Entity;
+
+use entity\sword\entity\Sword;
 
 class Main extends PluginBase{ 
 
+  public function onLoad(){
+    $this->getLogger()->info("Loading Throwable Sword");
+  }
+    
   public function onEnable(){
-    $this->getLogger()->info("ThrowableSword Enabled");
+    $this->getLogger()->info("Enabled Throwable Sword");
+    Entity::registerEntity(Sword::class, true);
+  }
+  
+  public function onDisable(){
+    $this->getLogger()->info("Disabled Throwable Sword");   
   }
 
 }
