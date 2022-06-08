bf5a2a2faa4cbb6920940115a22fa40bd20985e9
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-06-08 15:20:40 +0200

    ?

--- src/entity/sword/tasks/otherSwordTask.php
+++ src/entity/sword/tasks/otherSwordTask.php
@@ -7,18 +7,20 @@ use entity\sword\other\other;
 use entity\sword\entity\Sword;
 
 class otherSwordTask extends Task{
+    
+    public $plugin;
+    public $sword;
 
     public function __construct(other $plugin, Sword $sword){
         $this->plugin = $plugin;
         $this->sword = $sword;
     }
 
-    public function onRun(int $ct){
+    public function onRun(){
         if(!$this->sword->isClosed()){
             foreach($this->plugin->players as $player){
                 if($this->sword->asVector3()->distance($player) < 5){
-                   $this->Damage(1) 
-                    $this->plugin->plugin->getScheduler()->scheduleDelayedTask(new despawnsword($this->sword), 0)
+                    $this->plugin->getScheduler()->scheduleDelayedTask(new despawnsword($this->sword), 0)
                 }
             }
         }
