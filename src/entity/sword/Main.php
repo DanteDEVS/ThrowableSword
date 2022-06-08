f1c2a470041585b2a64c2e67086526a20425a52b
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-06-08 15:23:02 +0200

    rbetter changes?

--- src/entity/sword/Main.php
+++ src/entity/sword/Main.php
@@ -5,22 +5,27 @@ namespace entity\sword;
 use pocketmine\plugin\PluginBase;
 use pocketmine\entity\Entity;
 use pocketmine\entity\EntityFactory;
+use pocketmine\entity\EntityDataHelper;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\world\World;
 
 use entity\sword\entity\Sword;
 
 class Main extends PluginBase{ 
 
   public function onLoad() : void{
-    $this->getLogger()->info("Loading Throwable Sword");
+      $this->getLogger()->info("Loading Throwable Sword");
   }
     
   public function onEnable() : void{
-    $this->getLogger()->info("Enabled Throwable Sword");
-    EntityFactory::register(Sword::class, true);
+      $this->getLogger()->info("Enabled Throwable Sword");
+	    EntityFactory::getInstance()->register(Sword::class, function(World $world, CompoundTag $nbt) : Sword{
+		          return new SwordEntity(EntityDataHelper::parseLocation($nbt, $world), null, $nbt);
+	    }, ['Sword']);        
   }
   
   public function onDisable() : void{
-    $this->getLogger()->info("Disabled Throwable Sword");   
+      $this->getLogger()->info("Disabled Throwable Sword");   
   }
 
 }
